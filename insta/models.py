512694b73232ad1cb8ecb3f76882614d8454e1c0
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-09 16:06:17 +0300

    creates the image model

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name =models.CharField(max_length =30)
+    image_caption =models.CharField(max_length =50)
+    image = models.ImageField(upload_to = 'images/',blank = True)
+    profile = models.ForeignKey('Profile', on_delete = models.CASCADE,null='True', blank=True)
+    comments = models.TextField(max_length=100)
+    likes = models.Value(default = 0)
