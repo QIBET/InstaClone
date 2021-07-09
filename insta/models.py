0ece9e1a10dd1981d69f945352c060e4f2cf9906
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-09 16:14:05 +0300

    creates profile model

--- insta/models.py
+++ insta/models.py
@@ -5,6 +5,17 @@ class Image(models.Model):
     image_name =models.CharField(max_length =30)
     image_caption =models.CharField(max_length =50)
     image = models.ImageField(upload_to = 'images/',blank = True)
-    profile = models.ForeignKey('Profile', on_delete = models.CASCADE,null='True', blank=True)
     comments = models.TextField(max_length=100)
-    likes = models.Value(default = 0)
+    likes = models.PositiveBigIntegerField(default = 0)
+    profile = models.ForeignKey('Profile', on_delete = models.CASCADE,null='True', blank=True)
+
+
+    def __str__(self):
+        return self.image_name
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'images/',blank = True)
+    bio = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.profile_photo
