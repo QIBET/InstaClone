69d8fccc96a1e86afad9367d1c720e4d82d58882
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-11 17:14:48 +0300

    creates methods to return profile and image details

--- insta/models.py
+++ insta/models.py
@@ -8,8 +8,8 @@ class Image(models.Model):
     image_caption =models.CharField(max_length =50)
     image = models.ImageField(upload_to = 'images/',blank = True)
     comments = models.TextField(max_length=100)
-    user = models.ForeignKey('Profile', on_delete = models.CASCADE,null='True', blank=True)
     likes = models.IntegerField(default=0)
+    user = models.ForeignKey('Profile', on_delete = models.CASCADE,null='True', blank=True)
     
     def __str__(self):
         return self.image_name
@@ -19,6 +19,10 @@ class Image(models.Model):
 
     def delete_image(self):
         self.delete()
+    @classmethod
+    def get_images(cls):
+        all_pics=cls.objects.all()
+        return all_pics
 
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to = 'profilepics/',blank = True,)
@@ -33,6 +37,10 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+    @classmethod
+    def get_profiles(cls):
+        profiles=cls.objects.all()
+        return profiles
 
 class Follow(models.Model):
     follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
